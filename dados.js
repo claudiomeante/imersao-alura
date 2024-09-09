937bf2ce99be616e6d4413b4827ed293d5f7f4d6
claudiomeante <claudio_meante@live.com>
2024-09-08 21:14:01 -0300

    dados.js

--- dados.js
+++ dados.js
@@ -2,17 +2,20 @@ let dados = [
     {
         titulo: "Rayssa Leal",
         descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, trazendo o skate para o centro das atenções. Sua história de superação e paixão pelo esporte a tornaram um ícone para a nova geração.",
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
     },
     {
         titulo: "Beatriz Souza",
         descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro em sua categoria. Sua técnica apurada e determinação a colocaram no topo do pódio, inspirando jovens atletas a seguirem seus passos.",
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo jodoka"
     },
     {
         titulo: "Rebeca Andrade",
         descricao: "Rebeca Andrade é uma ginasta brasileira que brilhou nos Jogos Olímpicos de Paris 2024, conquistando diversas medalhas. Sua versatilidade e força física a tornaram uma das principais atletas da ginástica artística mundial.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ginástica ouro"
     }
 ];
 
